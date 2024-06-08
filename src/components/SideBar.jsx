a4728c1872dd3bf615111e7fd687d6af4a20f8c7
Maduranga Lakshan <170263946+MadurangaLakshan@users.noreply.github.com>
2024-06-08 23:44:32 +0530

    Improved mobile responsiveness

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
 import AllChats from "./AllChats";
+import { ChatContext } from "../contexts/ChatContext";
 
 const SideBar = () => {
+  const { data } = useContext(ChatContext);
+
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${data.isFullscreen ? "" : "fullscreen"}`}>
       <NavBar />
       <SearchBar />
       <AllChats />
